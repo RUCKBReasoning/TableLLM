b248306e52e9e36e5f95bc9cd6d4e7e2093fdb45
Sijia Luo <97578023+lsjia@users.noreply.github.com>
2025-08-16 15:31:20 +0800

    Update preprocess_utils.py

--- finetune/preprocess_utils.py
+++ finetune/preprocess_utils.py
@@ -30,8 +30,7 @@ class InputOutputDataset(Dataset):
     def __getitem__(self, i) -> dict:
         data_item = self.data[i]
 
-        # CodeLlama follow the instruct format of Llama2
-        PROMPT_FORMAT = "[INST]{instruction}[/INST]"
+        PROMPT_FORMAT = "<|start_header_id|>user<|end_header_id|>\n\n{instruction}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
         prompt = PROMPT_FORMAT.format_map({'instruction': data_item["prompt"]})
 
         a_ids = self.tokenizer.encode(
@@ -40,26 +39,25 @@ class InputOutputDataset(Dataset):
             truncation=True,
             max_length=self.max_source_length,
         )
+        PROMPT_FORMAT = "{response}<|eot_id|>"
+        response = PROMPT_FORMAT.format_map({'response': data_item["response"]})
         b_ids = self.tokenizer.encode(
-            text=data_item["response"],
+            text=response,
             add_special_tokens=False,
             truncation=True,
             max_length=self.max_target_length,
         )
-
         context_length = len(a_ids)
-        input_ids = a_ids + b_ids + [self.tokenizer.eos_token_id]
+        input_ids = a_ids + b_ids
         labels = (
-            [self.tokenizer.pad_token_id] * context_length
+            [-100] * context_length
             + b_ids
-            + [self.tokenizer.eos_token_id]
         )
 
         pad_len = self.max_seq_length - len(input_ids)
         attention_mask = [1] * len(input_ids) + [0] * pad_len
         input_ids = input_ids + [self.tokenizer.pad_token_id] * pad_len
-        labels = labels + [self.tokenizer.pad_token_id] * pad_len
-        labels = [(l if l != self.tokenizer.pad_token_id else -100) for l in labels]
+        labels = labels + [-100] * pad_len
 
         assert len(input_ids) == len(
             labels
